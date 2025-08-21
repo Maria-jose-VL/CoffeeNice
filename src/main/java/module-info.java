c4e09865ad0f19605dd61db1d953fbada23d3ffc
Maria-jose-VL <mariaj.valencial@uqvirtual.edu.co>
2025-08-21 09:21:21 -0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module co.edu.uniquidio.coffeenice.coffeenice {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens co.edu.uniquidio.coffeenice.coffeenice to javafx.fxml;
+    exports co.edu.uniquidio.coffeenice.coffeenice;
+}
